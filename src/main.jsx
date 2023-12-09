f9d1001788545c7ab3374b4e4491fb57de2f709d
trong hieu <hieu745233@gmail.com>
2023-12-09 07:22:50 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,7 @@ import { store, persistor } from './pages/redux/stores/index.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <React.StrictMode>
         <App />
-      </React.StrictMode>
     </PersistGate>
   </Provider>,
 );
--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -4,7 +4,7 @@ import ProductCart from "./productCart";
 
 import styles from "../cart/productCart/ProductCart.module.scss";
 import classNames from "classnames/bind";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { createOrderApi, thanhtoanMomo } from "../../Api";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -13,16 +13,16 @@ const cx = classNames.bind(styles);
 function Cart() {
   const cartList = useSelector(cartListSelectors);
   const navigate = useNavigate();
-  const idUser = localStorage.getItem('id') || undefined;
-  let detailOrder = cartList.map(item => ({
+  const idUser = localStorage.getItem("id") || undefined;
+  let detailOrder = cartList.map((item) => ({
     id: item.id,
     soluong: item.quantity,
-    gia: item.dongia.replace(',', ''),
-    tong: (item.quantity * parseFloat(item.dongia.replace(',', ''))).toString(),
+    gia: item.dongia.replace(",", ""),
+    tong: (item.quantity * parseFloat(item.dongia.replace(",", ""))).toString(),
     ghichu: item.ghichu,
     mahoadon: "",
-    masanpham: item.id
-  }))
+    masanpham: item.id,
+  }));
   const totalAmount = detailOrder.reduce((total, item) => {
     // Convert 'tong' to a number and add it to the total
 
@@ -34,21 +34,26 @@ function Cart() {
     setOrder((prevOrder) => ({ ...prevOrder, tongtien: totalAmount }));
   }, [totalAmount]);
 
+  const url = new URL(window.location.href);
+  const partnerCode = url.searchParams.get("partnerCode") || null;
   const [order, setOrder] = useState({
-    hoten: '',
-    diachi: '',
-    sdt: '',
-    trangthai: 'Pending',
+    hoten: "",
+    diachi: "",
+    sdt: "",
+    trangthai: "Pending",
     ngaydathang: "2023-11-29 04:30:43",
     mathanhtoan: 1,
     mavanchuyen: 1,
     makhachhang: idUser,
     tongtien: totalAmount,
     details: detailOrder,
-    ghichu: '',
-    quan: 'Quận 2'
-  })
+    ghichu: "",
+    quan: "Quận 2",
+  });
+  
+
   console.log(order);
+
   useEffect(() => {
     // Optionally, you can update the order details when cartList changes
     setOrder((prevOrder) => ({ ...prevOrder, details: detailOrder }));
@@ -62,30 +67,112 @@ function Cart() {
   const handleCreateOrder = async () => {
     try {
       if (idUser === undefined) {
-        navigate("/login")
+        navigate("/login");
       }
-      const response = await createOrderApi(order); // Use createOrderApi function
-      const isConfirmed = window.confirm("Bạn chắn chắc muốn đặt món này?");
+      const response = await createOrderApi(order);
+      var isConfirmed = null; // Use createOrderApi function
+      if (partnerCode == null) {
+        isConfirmed = window.confirm("Bạn chắn chắc muốn đặt món này?");
+      }
       if (isConfirmed && response.success) {
-        console.log('Order created successfully');
-        console.log('Order ID:', response.id);
-        localStorage.removeItem('persist:root')
-        navigate("/user/order/payment")
+        console.log("Order created successfully");
+        console.log("Order ID:", response.id);
+        localStorage.removeItem("persist:root");
+        navigate("/user/order/payment");
         // Optionally, clear the cart or perform other actions
       } else {
-        console.error('Failed to create order:', response.message);
+        console.error("Failed to create order:", response.message);
       }
     } catch (error) {
-      console.error('Error creating order:', error.message);
+      console.error("Error creating order:", error.message);
     }
   };
-
-  const handleMomo = () => {
-    axios.post("http://localhost:3001/api/thanhtoan/momo", {}).then(res => {
-      if (res) {
-        console.log(res);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (partnerCode) {
+        try {
+          const hoten = localStorage.getItem("hoten");
+          const diachi = localStorage.getItem("diachi");
+          const sdt = localStorage.getItem("sdt");
+          const ghichu = localStorage.getItem("ghichu");
+          const quan = localStorage.getItem("quan");
+          const tongtien = localStorage.getItem("tongtien");
+          const details = localStorage.getItem("details");
+  
+          setOrder((prevOrder) => ({
+            ...prevOrder,
+            hoten: hoten || prevOrder.hoten,
+            diachi: diachi || prevOrder.diachi,
+            sdt: sdt || prevOrder.sdt,
+            ghichu: ghichu || prevOrder.ghichu,
+            quan: quan || prevOrder.quan,
+            tongtien: tongtien || prevOrder.tongtien,
+            details: details || prevOrder.details,
+            mathanhtoan: 2,
+          }));
+  
+          localStorage.removeItem("hoten");
+          localStorage.removeItem("diachi");
+          localStorage.removeItem("sdt");
+          localStorage.removeItem("ghichu");
+          localStorage.removeItem("quan");
+          localStorage.removeItem("tongtien");
+          localStorage.removeItem("details");
+  
+        } catch (error) {
+          console.log(error);
+        }
       }
-    })
+    };
+  
+    fetchData();
+  
+    // Check if the order has all the required data before calling handleCreateOrder
+  }, [partnerCode]);
+
+  useEffect(()=>{
+    if (
+      order.hoten &&
+      order.diachi &&
+      order.sdt &&
+      order.ghichu &&
+      order.quan &&
+      order.tongtien &&
+      order.details &&
+      order.mathanhtoan == 2
+    ) {
+      console.log(order.details);
+      handleCreateOrder();
+    }
+  },[order.details])
+  const handleMomo = async (event) => {
+    try {
+      event.preventDefault();
+      const updatedOrder = {
+        ...order,
+        details: detailOrder,
+      };
+
+      const response = await axios.post(
+        "http://localhost:3001/api/thanhtoan/momo",
+        {}
+      );
+      if (response.data.url) {
+        // Lưu vào localStorage
+        localStorage.setItem("hoten", updatedOrder.hoten);
+        localStorage.setItem("diachi", updatedOrder.diachi);
+        localStorage.setItem("sdt", updatedOrder.sdt);
+        localStorage.setItem("ghichu", updatedOrder.ghichu);
+        localStorage.setItem("quan", updatedOrder.quan);
+        localStorage.setItem("tongtien", updatedOrder.tongtien);
+        localStorage.setItem("details", JSON.stringify(updatedOrder.details));
+
+        // Chuyển hướng sau khi lưu vào localStorage
+        window.location.href = response.data.url;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
@@ -111,22 +198,42 @@ function Cart() {
               />
             );
           })}
-
         </div>
 
         <div className={cx("right-container")}>
           <form action="">
             <h3 className="title-cart">Thông tin đơn hàng</h3>
             <label htmlFor="firstName">Họ tên:</label>
-            <input type="text" id="firstName" name="hoten" placeholder="Nhập tên người nhận" required onChange={handleInputChange} />
+            <input
+              type="text"
+              id="firstName"
+              name="hoten"
+              placeholder="Nhập tên người nhận"
+              required
+              onChange={handleInputChange}
+            />
 
             <label htmlFor="phone">Số điện thoại:</label>
-            <input type="telephone" id="phone" name="sdt" placeholder="Số điện thoại người nhận" required onChange={handleInputChange} />
+            <input
+              type="telephone"
+              id="phone"
+              name="sdt"
+              placeholder="Số điện thoại người nhận"
+              required
+              onChange={handleInputChange}
+            />
 
             <div className={cx("edit-address-cart")}>
               <div className={cx("left-div-address")}>
                 <label htmlFor="address">Địa chỉ:</label>
-                <input type="text" id="address" name="diachi" placeholder="Số nhà, tòa nhà, đường..." required onChange={handleInputChange} />
+                <input
+                  type="text"
+                  id="address"
+                  name="diachi"
+                  placeholder="Số nhà, tòa nhà, đường..."
+                  required
+                  onChange={handleInputChange}
+                />
               </div>
               <div className={cx("edit-option")}>
                 <label htmlFor="dropdown">Quận:</label>
@@ -141,7 +248,13 @@ function Cart() {
             </div>
 
             <label htmlFor="note">Ghi chú:</label>
-            <input type="text" id="note" name="ghichu" required onChange={handleInputChange} />
+            <input
+              type="text"
+              id="note"
+              name="ghichu"
+              required
+              onChange={handleInputChange}
+            />
 
             <div className={cx("btn-cart-option")}>
               <div className={cx("button-cash")}>
@@ -150,9 +263,7 @@ function Cart() {
                 </button>
               </div>
               <div className={cx("button-momo")}>
-                <button onClick={handleMomo}>
-                  Online thanh toán
-                </button>
+                <button onClick={handleMomo}>Online thanh toán</button>
               </div>
             </div>
           </form>
